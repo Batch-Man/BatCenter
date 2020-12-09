1d9721919a32974af489e9c11139b7a439f20dfa
TheKvc <karanveerchouhan@gmail.com>
2020-12-09 18:33:12 +0530

    v.2.0

--- Bat.bat
+++ Bat.bat
@@ -61,6 +61,9 @@ For %%A in ("Json" "plugins" "Files" "Index" "Zips") do (If Not Exist "!_path!\%
 Set "path=%path%;%_path%;%_path%\Files;%_path%\plugins;%cd%;%cd%\files"
 CD /d "%_path%"
 
+REM Adjusting and transferring all files to new path...
+If Exist "%SystemDrive%\system\Bat\hosts.txt" (Start "" /SHARED /WAIT /B Transfer.bat && Echo. >"FirstLaunch.txt")
+
 If exist "FirstLaunch.txt" (
 	Echo Setting up Bat-Center by Kvc...
 	Del /F /q "FirstLaunch.txt" >nul 2>nul
@@ -378,9 +381,6 @@ If !_online_ver! GTR !_ver! (
 	) >"!Temp!\UpdateBat.bat"
 )
 
-REM Adjusting and transferring all files to new path...
-If Exist "%SystemDrive%\system\Bat\hosts.txt" (Call Transfer.bat)
-
 REM Removing Older Index Files...
 Del /f /q "Index\*.*" >nul 2>nul
 
@@ -428,7 +428,7 @@ If not exist "Files\_APIAccessTime.txt" (
 	IF !_TempCount! GEQ 180 (Echo.This file limits API calls...>"Files\BlockUpdate.txt")
 	)
 REM Updating BatCenter in case, if there is an update...
-If Defined _UpdateBat (Start "" /SHARED "!Temp!\UpdateBat.bat")
+If Defined _UpdateBat (Start "" /SHARED /B "!Temp!\UpdateBat.bat")
 Exit /b 0
 
 REM ============================================================================
